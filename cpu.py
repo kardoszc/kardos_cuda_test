26dca88a0a13453603bf0c9fffad716a9a2889f4
kardoszc <kardos_zc@outlook.com>
2016-06-24 18:21:40 +0800

    create

---
+++ cpu.py
@@ -0,0 +1,42 @@
+import pycuda.autoinit
+import pycuda.driver as drv
+import numpy
+import time
+import threading
+from pycuda.compiler import SourceModule
+
+def test(i,dest,a,b):
+	dest[i] = a[i] * b[i]
+
+mod = SourceModule("""
+__global__ void multiply_them(float *dest, float *a, float *b)
+{
+  int n = 0;
+  for(n=0;n<=9999;n+=1) 
+  {
+  const int i = threadIdx.x + n * 1000;
+  dest[i] = a[i] * b[i];
+  }
+}
+""")
+
+multiply_them = mod.get_function("multiply_them")
+
+a = numpy.random.randn(1000*1000).astype(numpy.float32)
+b = numpy.random.randn(1000*1000).astype(numpy.float32)
+
+dest = numpy.zeros_like(a)
+
+threadpool=[]
+
+start = time.time()
+for i in xrange(1000*1000):
+    th = threading.Thread(target= test,args= (i,dest,a,b))
+    threadpool.append(th)
+print time.time() - start
+for th in threadpool:
+    th.start()
+print time.time() - start
+for th in threadpool :
+    threading.Thread.join( th )
+print time.time() - start
---
+++ tutorial.py
@@ -0,0 +1,38 @@
+import pycuda.autoinit
+import pycuda.driver as drv
+import numpy
+import time
+from pycuda.compiler import SourceModule
+mod = SourceModule("""
+__global__ void multiply_them(float *dest, float *a, float *b)
+{
+  int n = 0;
+  for(n=0;n<=9999;n+=1) 
+  {
+  const int i = threadIdx.x + n * 1000;
+  dest[i] = a[i] * b[i];
+  }
+}
+""")
+
+multiply_them = mod.get_function("multiply_them")
+
+a = numpy.random.randn(1000*10000).astype(numpy.float32)
+b = numpy.random.randn(1000*10000).astype(numpy.float32)
+
+dest = numpy.zeros_like(a)
+
+
+# cuda计算
+start = time.time()
+multiply_them(
+        drv.Out(dest), drv.In(a), drv.In(b),
+        block=(1000,1,1), grid=(1,1))
+print time.time() - start
+
+
+
+# for i in range(len(a)):
+# 	dest[i] = a[i] * b[i]
+
+# print time.time() - start
